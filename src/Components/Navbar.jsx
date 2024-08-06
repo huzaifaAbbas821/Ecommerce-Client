2fa4d3b0408bb1041194690971f881b0592bc134
huzaifaAbbas821 <huzaifaabbas8121@gmail.com>
2024-08-07 01:59:05 +0500

    Changes 3

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -208,7 +208,7 @@ console.log(response)
       </div>
       <div className="search  xs:hidden  bg-gray-400  px-3 cursor-pointer rounded-sm overflow-hidden flex items-center justify-between gap-0">
         <input
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
           type="text"
           placeholder="Search.."
           className="w-[90%] py-2 text-black rounded-lg placeholder:text-black bg-gray-400 outline-none border-0 border-transparent text-base"
